a580a1e5fcedfad850e234ea8c60672d1c7fe4b3
feliperama <feliperama@gmail.com>
2020-04-02 10:21:19 -0400

    fix rspec tests for pages controller

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe PagesController, type: :controller do
+RSpec.describe PagesController, type: :request do
+  before do
+    host! 'thinkfic.example.com'
+  end
 
-  describe "GET #home" do
-    it "returns http success" do
-      get :home
-      expect(response).to have_http_status(:success)
+  describe "GET /" do
+    context "When the user is not logged in" do
+      it "should present the necessary login functions" do
+        get '/'
+
+        expect(response).to have_http_status(:success)
+        expect(response.body).to include "users/sign_in"
+        expect(response.body).to include "users/sign_up"
+        expect(response.body).to include "facebook"
+      end
     end
+
+    context "When the user is logged in" do
+      let(:user) { create(:user) }
+
+      before do
+        allow_any_instance_of(described_class)
+          .to receive(:current_user)
+          .and_return(user)
+      end
+
+      it "should present logout function" do
+        get '/'
+
+        expect(response).to have_http_status(:success)
+        expect(response.body).to_not include "users/sign_in"
+        expect(response.body).to_not include "users/sign_up"
+        expect(response.body.downcase).to include "logout"
+      end
+    end
+
   end
 
 end
