05ec34ef942b934fee77f25d7a4d9a14dfbc3b8e
feliperama <feliperama@gmail.com>
2020-04-02 14:59:24 -0400

    fix curl base path for production and development

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   def home
     @new_token = current_user.generate_token if current_user
+
+    if Rails.env == "production"
+      @curl_base_path = "https://cherry-sundae-20794.herokuapp.com"
+    else
+      @curl_base_path = "http://localhost"
+    end
   end
 end
